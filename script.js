0fcf7345d1486d0cb3bd8dbc0ec11cec6d825f55
Den Sun Li <111985236+densuni@users.noreply.github.com>
2023-03-03 22:15:00 +0900

    Add a grid size range slider

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 const gridContainer = document.querySelector(".grid-container");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("range-value");
+output.innerHTML = slider.value; 
 
-function createGrid() {
-  for(let i = 0; i < 256; i++) {
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+slider.addEventListener("mouseup", changeGridSize);
+
+createGrid(16);
+
+function changeGridSize() {
+  let currentSize = slider.value;
+  createGrid(currentSize);
+  resetGrid();
+}
+
+function resetGrid() {
+  let divs = document.querySelectorAll(".grid-item");
+  divs.forEach((div) => div.style.backgroundColor = "white");
+}
+
+function createGrid(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let divNum = size * size;
+
+  for(let i = 0; i < divNum; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     gridContainer.appendChild(gridItem);  
   }
-}
 
-createGrid();
+  const divs = document.querySelectorAll(".grid-item");
 
-const divs = document.querySelectorAll(".grid-item");
-
-Array.from(divs).forEach(div => {
-  div.addEventListener("mouseenter", (e) => {
-      e.target.style.backgroundColor = "black";
+  Array.from(divs).forEach(div => {
+    div.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "black";
+    });
   });
-});
+}
